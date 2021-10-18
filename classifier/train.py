b20ecfc78573611196849d7a4e23b1fa51889726
jake <you@example.com>
2021-10-18 21:21:30 +0800

    Add train.py

--- classifier/train.py
+++ classifier/train.py
@@ -1,3 +1,102 @@
-from efficientnet_pytorch import EfficientNet
+from config import *
 
-model = EfficientNet.from_pretrained('efficientnet-b0')
+import matplotlib.pyplot as plt
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.data import AUTOTUNE
+import tensorflow as tf
+
+
+def train_val_split(DATA_DIR, IMG_SIZE):
+    val_data = image_dataset_from_directory(
+                    DATA_DIR,
+                    labels="inferred",
+                    label_mode="binary",
+                    color_mode="rgb",
+                    batch_size=32,
+                    image_size=(IMG_SIZE, IMG_SIZE),
+                    validation_split=0.2,
+                    subset="training",
+                    seed=1
+                )
+
+    train_data = image_dataset_from_directory(
+                    DATA_DIR,
+                    labels="inferred",
+                    label_mode="binary",
+                    color_mode="rgb",
+                    batch_size=32,
+                    image_size=(IMG_SIZE, IMG_SIZE),
+                    validation_split=0.2,
+                    subset="validation",
+                    seed=1
+                )
+
+    train_data = train_data.map(lambda x, y: (x, tf.one_hot(y, depth=NUM_CLASSES)))
+    val_data = val_data.map(lambda x, y: (x, tf.one_hot(y, depth=NUM_CLASSES)))
+
+    train_data = train_data.cache().prefetch(buffer_size=AUTOTUNE)
+    val_data = val_data.cache().prefetch(buffer_size=AUTOTUNE)
+
+    return train_data, val_data
+
+
+def plot_hist(hist, metric="accuracy"):
+    """plot results, metric can be 'accuracy' or 'loss' """
+    plt.plot(hist.history[metric])
+    plt.plot(hist.history[f"val_{metric}"])
+    plt.title("model accuracy")
+    plt.ylabel(metric)
+    plt.xlabel("epoch")
+    plt.legend(["train", "validation"], loc="upper left")
+    plt.show()
+
+
+def model_arch(NUM_CLASSES, IMG_SIZE):
+    """efficientnet transfer learning"""
+    inputs = layers.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
+    img_augmentation = Sequential(
+        [
+            layers.RandomRotation(factor=0.15),
+            layers.RandomTranslation(height_factor=0.1, width_factor=0.1),
+            layers.RandomFlip(),
+            layers.RandomContrast(factor=0.1),
+        ],
+        name="img_augmentation",
+    )
+
+    x = img_augmentation(inputs)
+    # model = EfficientNetB0(include_top=False, input_tensor=x, weights="imagenet")
+    model = EfficientNetB0(include_top=False, input_tensor=x, weights='model/efficientnetb0_notop.h5')
+
+    # Freeze the pretrained weights
+    model.trainable = False
+
+    # Rebuild top
+    x = layers.GlobalAveragePooling2D(name="avg_pool")(model.output)
+    x = layers.BatchNormalization()(x)
+
+    top_dropout_rate = 0.2
+    x = layers.Dropout(top_dropout_rate, name="top_dropout")(x)
+    outputs = layers.Dense(NUM_CLASSES, activation="softmax", name="pred")(x)
+
+    # Compile
+    model = tf.keras.Model(inputs, outputs, name="EfficientNet")
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2)
+    model.compile(
+        optimizer=optimizer, 
+        loss="binary_crossentropy", 
+        metrics=["accuracy"]
+    )
+    return model
+
+
+if __name__ == "__main__":
+    train_data, val_data = train_val_split(DATA_DIR, IMG_SIZE)
+    model = model_arch(NUM_CLASSES, IMG_SIZE)
+    hist = model.fit(train_data, 
+                     epochs=EPOCH, 
+                     validation_data=val_data, 
+                     verbose=1)
