2889f24ab367116ad885a856d5085f229b3fcb4b
_ <teo_siyang@imda.gov.sg>
2021-10-19 12:21:11 +0000

    Add learning rate

--- classifier/config.py
+++ classifier/config.py
@@ -1,5 +1,12 @@
+# training configs
+
 DATA_DIR = "img/"
 IMG_SIZE = 224 # fixed for each efficientnet model
 NUM_CLASSES = 2
 EPOCH = 50
 LR = 1e-2 # learning rate
+
+
+# inference configs
+
+MODEL_DIR = "model/20211019"
